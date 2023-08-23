4fc2568243e043d3d5e4c3e2fbc58cde3b093815
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-08-22 21:09:49 -0300

    implemented database's basic query function

---
+++ app/src/database.ts
@@ -0,0 +1,43 @@
+import postgres from 'postgres';
+import path from 'path';
+import fs from 'fs';
+
+class Database {
+
+    private static instance: Database;
+
+    private constructor() {
+        // Singleton class
+    }
+
+    public static getInstace(): Database {
+        if (!Database.instance) {
+            Database.instance = new Database();
+        }
+        return Database.instance;
+    }
+
+    private options = {
+
+        host: '127.0.0.1',
+        port: 5000,
+
+        database: 'appdb',
+        username: 'dbuser',
+        password: 'appdb',
+
+    };
+
+    private sql = postgres(this.options);
+
+    public async query(query: string): Promise<any> {
+        try {
+            const result = await this.sql.unsafe(query).then((res: any) => Object.values(res[0])[0]);
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+export default Database;
